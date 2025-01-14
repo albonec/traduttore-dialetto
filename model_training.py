da9916e9fae06cac6c69c87f82f0815fc0ca3a30
albonec <agbonecchi@gmail.com>
2025-01-13 22:04:21 -0800

    added and formatted dictionary

--- model_training.py
+++ model_training.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # Step 1: Data Collection
 url = 'https://sites.google.com/site/dialetdebresa/dizionario-bresciano-italiano'
-response = requests.get(url)
+html = requests.get(url)
 
 def remove_tags(html):
     soup = BeautifulSoup(html, "html.parser")
@@ -23,7 +23,7 @@ def remove_parentheses(text):
         text = text[:start] + text[end:]
     return text
 
-raw_text = remove_tags(response.text)
+raw_text = remove_tags(html.text)
 clean_text = remove_sections(raw_text, ["Dialèt de Brèsa (dialetto bresciano) - Dizionario bresciano-italiano Search this site Embedded Files Skip to main content Skip to navigation Dialèt de Brèsa (dialetto bresciano) Il dialetto bresciano home_bs Home_en Dizionario bresciano-italiano Grammatica Proverbi bresciani, antichi e moderni Toponomastica bresciana Dialèt de Brèsa (dialetto bresciano) Il dialetto bresciano home_bs Home_en Dizionario bresciano-italiano Grammatica Proverbi bresciani, antichi e moderni Toponomastica bresciana More Il dialetto bresciano home_bs Home_en Dizionario bresciano-italiano Grammatica Proverbi bresciani, antichi e moderni Toponomastica bresciana Dizionario bresciano-italiano Dizionario Bresciano-Italiano Ecco un dizionario di base. Aggiungete il vostro vocabolo preferito in commento (o mandate una mail qui ) e verrà aggiunto alla lista. Qui trovate anche: una guida alla pronuncia e all'ortografia vedete qui; un' introduzione al dialetto e al sito , una lista di proverbi bresciani con traduzione in italiano la lista completa e corretta dei i nomi dei comuni bresciani in dialetto . (a piè di pagina l'elenco delle abbreviazioni )",
                                                        "[A] [B] [C] [D] [E] [F] [G] [H] [I] [L] [M] [N] [N] [O] [P] [Q] [R] [S] [T] [U] [V] [Z]",
                                                        "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z",
@@ -32,6 +32,8 @@ clean_text = remove_sections(raw_text, ["Dialèt de Brèsa (dialetto bresciano) 
 
 cleaner_text = remove_sections(' '.join(remove_parentheses(clean_text).split()), ["ai a: l dialetto bresciano rammatica roverbi bresciani, antichi e moderni oponomastica bresciana ella trascrizione dei termini dialettali si sono usate le regole fonetiche definite per tutti i documenti di questo sito. ra parentesi sono riportate le descrizioni grammaticali e di utilizzo dei termini, abbreviate secondo il seguente schema: agg. = aggettivo art. = articolo avv. = avverbio cong. = congiunzione det. = determinativo escl. = esclamazione est. = per estensione significa anche fam. = familiare fig. = figurato n.f. = sostantivo maschile n.m. = sostantivo femminile off! = offensivo, da evitare pl. = usato solo al plurale pr. = pronome prep. = preposizione rafforz. = rafforzativo sing. = usato solo al singolare spec. = specialmente usato v.in. = verbo intransitivo v.tr. = verbo transitivo ! = molto volgare, da evitare ~ = sostituisce la parola definita 1998-2013 arco orzati . oogle ites eport abuse age details age updated oogle ites eport abuse", "ai a: l dialetto bresciano rammatica roverbi bresciani, antichi e moderni oponomastica bresciana"])
 
+out_file = open("dict.txt", "w")
+out_file.write(cleaner_text)
 print(cleaner_text)
 
 bresciano_words = []
